9946badfb6832ec14934cda5be42985ec7d7973b
Mark Cruz <markchriscruz@yahoo.com>
2018-05-09 22:50:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,6 +16,7 @@ class Pokemon
     def self.find(id, db)
     name = db.execute("SELECT name FROM pokemon WHERE id = #{id}").flatten[0]
     type = db.execute("SELECT type FROM pokemon WHERE id = #{id}").flatten[0]
+    hp = db.execute("SELECT hp FROM pokemon WHERE id = #{id}").flatten[0]
     pokemonhash = {id: id, name: name, type: type, hp: hp, db: db}
     newPokemon = Pokemon.new(pokemonhash)
     return newPokemon
